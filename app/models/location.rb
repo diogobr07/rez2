f3587a6563d2785ef3aee968917d49142a183b81
diogo <diogobruno07@gmail.com>
2013-08-25 12:22:17 -0300

    add location model

---
+++ app/models/location.rb
@@ -0,0 +1,3 @@
+class Location < ActiveRecord::Base
+  attr_accessible :name, :tweet_id
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130818153031) do
+ActiveRecord::Schema.define(:version => 20130825151641) do
+
+  create_table "locations", :force => true do |t|
+    t.string   "name"
+    t.integer  "tweet_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "tweets", :force => true do |t|
     t.string   "status"
